e465d3c5bf1da334fed701ab2c9bbb433062541f
Gabriel Albuquerque <96128995+Gabr1elAlbuquerque@users.noreply.github.com>
2022-06-08 17:04:38 -0300

    Início do jogo de paciência

---
+++ Estudos/TrabalhoAlvaro/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+const unsigned int QuantCartas = 52, QuantNaipes = 4, QuantCartasNaipes = 13;
+
+typedef enum _Naipes_t{
+    Ouros = 0,
+    Espadilha = 1,
+    Paus = 2,
+    Copas = 3
+}Naipes_t;
+
+int main (void){
+
+}
