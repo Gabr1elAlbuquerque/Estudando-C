47dbfe424998556061face52b76a0d87d3a4bb7c
Gabriel Albuquerque <96128995+Gabr1elAlbuquerque@users.noreply.github.com>
2022-06-16 09:28:31 -0300

    Baralho

--- Estudos/TrabalhoAlvaro/teste.cpp
+++ Estudos/TrabalhoAlvaro/teste.cpp
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <iostream>
 
-int main(int argc, char const *argv[])
-{
-    const unsigned int TAMANHO = 13;
-    typedef struct _cartas
-    {
-        char letra;
-        int numero;
-    }cartas;
+const unsigned int TAMANHO = 52;
 
-    cartas v[TAMANHO];
-    for (int i = 0; i < TAMANHO; i++)
-    {
-           if (i < 4)
-           {
-                scanf("%c", &v[i]);}
-            scanf("%d", &v[i]);
+typedef enum _naipe_t{
+    paus,
+    ouro,
+    espadilha,
+    copas
+}naipe_t;
 
-           
+typedef struct _cartas_t{
+    int numeros, virada;
+    naipe_t naipe;
+}cartas_t;
+
+cartas_t baralho[TAMANHO];
+
+int main (int argc, char *argv[]){
+    for(int c = 0; c < TAMANHO; c++){
+        baralho[c].numeros = (c % 13) + 1;
+        baralho[c].naipe = (naipe_t) (c / 13);
+        printf("%i %i %i\n", c, baralho[c].numeros, baralho[c].naipe);
     }
-    
-    
-    return 0;
+    /* No printf, a primeira coluna é o indice, na segunda o número da carta(1 = A, 11 = J, 12 = Q, 13 = K), na terceira é o naipe que foi definido no enum acima*/
 }
